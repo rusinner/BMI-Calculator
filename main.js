67d8e4a4bfc0a39a93389a07d0e6208369fd198c
Theologos Mavropoulos <rusinner88@gmail.com>
2022-10-03 14:57:45 +0300

    final
    needs the convertion to lbs and meters

--- main.js
+++ main.js
@@ -18,25 +18,43 @@ const toggleDialog = () => {
 
 //calculate BMI function
 const calculateBMI = () =>{
-    const height = document.getElementsByClassName('height')[0].value;
+    
+
+    const height = document.getElementsByClassName('height')[0].value / 100;
     const weight = document.getElementsByClassName('weight')[0].value;
+    const option = document.getElementsByTagName('option');
+   
     const dialogTitle =  document.getElementsByClassName('dialog-title')[0];
     const dialogResult =  document.getElementsByClassName('res')[0];
     const dialogResultIcon = document.getElementsByClassName('result-icon-wrap')[0];
     const result = weight / (height * height);
+    if(isNaN(height) || isNaN(weight) ){
+        dialogTitle.innerHTML = 'invalid values entered';
+        dialogResult.innerHTML = '';
+        dialogResultIcon.innerHTML = `<i class = "fas fa-times-circle danger"></i>`
+    }else{
 
     if(result < 18.5){
+        // dialogTitle.innerHTML = 'BMI Index : ${result} '
         dialogResult.innerHTML = 'Result: Under weight';
-        dialogResultIcon.innerHTML = `<i class="fas fa-times-circle danger"></i>`
+        dialogResultIcon.innerHTML = `<i class = "fas fa-exclamation-circle danger"></i>`
     }else if (result >=18.5 && result <=24.9){
+        dialogTitle.innerHTML = 'BMI Index : ${result} '
         dialogResult.innerHTML = 'Result: Normal';
     }else if (result >=25 && result <=29.9){
+        // dialogTitle.innerHTML = 'BMI Index : ${result} '
         dialogResult.innerHTML = 'Result: Over weight';
+        dialogResultIcon.innerHTML = `<i class = "fas fa-exclamation-circle warn"></i>`
     }
     else if (result >=30 ){
+        // dialogTitle.innerHTML = 'BMI Index : ${result} ';
+        dialogResultIcon.innerHTML = `<i class = "fas fa-exclamation-circle danger"></i>`
         dialogResult.innerHTML = 'Result: Obesed';
     }
+    dialogTitle.innerHTML = `BMI Index :${Math.round(result)}`;
+}
     toggleDialog();
+    
     
     
 }
